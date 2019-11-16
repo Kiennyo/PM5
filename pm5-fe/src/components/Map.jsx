21a1f2285d88c11128c02beba5aff2df0d632de4
Vytautas Lukšys <vy.luksys@gmail.com>
2019-11-16 19:20:46 +0200

    did markers with fixed values, need to build it to import into web

--- pm5-fe/src/components/Map.jsx
+++ pm5-fe/src/components/Map.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
 
 class Map extends Component {
-    state = {
-        title: "map"
-    };
+  state = {
+    title: "map"
+  };
+  render() {
+  const tags = {
+    list: [
+      { id: 1, name: "Philip Morris Lietuva", lat: 55.6967, lng: 21.2002 },
+      { id: 2, name: "MARS", lat: 55.7027, lng: 21.3613 },
+      { id: 3, name: "Glassbel Baltic, UAB", lat: 55.691043, lng: 21.202709 },
+      { id: 4, name: "Orion Global Pet, UAB", lat: 55.690574, lng: 21.210001 },
+      { id: 4, name: "RETAL Baltic, UAB", lat: 55.688269, lng: 21.201974 },
+    ]
+  };
 
-    render() {
-        return (
-            <div>
-                <h1>{this.state.title}</h1>
-            </div>
-        );
-    }
+    return (
+      <div>
+          <h1>{this.state.title}</h1>
+        <ul>
+          {tags.list.map(reptile => (
+            <li>{reptile.name}</li>
+          ))}
+        </ul>
+        
+      </div>
+    );
+  }
 }
 
 export default Map;
--- pm5-fe/src/components/MapComponent.js
+++ pm5-fe/src/components/MapComponent.js
@@ -1,29 +1,141 @@
-import React from 'react'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import React from "react";
+import { Map, CircleMarker, Tooltip, TileLayer} from "react-leaflet";
 
 export default class MapComponent extends React.Component {
     state = {
-        lat: 51.505,
-        lng: -0.09,
-        zoom: 13,
+        lez: {
+            lat: 55.694,
+            lng: 21.1957,
+            zoom: 14
+        },
+        pmi: {
+            lat: 55.6967,
+            lng: 21.2002,
+            zoom: 14
+        },
+        mars: {
+            lat: 55.7027,
+            lng: 21.3613,
+            zoom: 14
+        },
+        gb: {
+            lat: 55.691043,
+            lng: 21.202709,
+            zoom: 14
+        },
+        ogp: {
+            lat: 55.690574,
+            lng: 21.210001,
+            zoom: 14
+        },
+        rb: {
+            lat: 55.688269,
+            lng: 21.201974,
+            zoom: 14
+        }
     };
 
     render() {
-        const position = [this.state.lat, this.state.lng];
+        const position = [this.state.lez.lat, this.state.lez.lng];
+        const pmi = [this.state.pmi.lat, this.state.pmi.lng];
+        const mars = [this.state.mars.lat, this.state.mars.lng];
+        const gb = [this.state.gb.lat, this.state.gb.lng];
+        const ogp = [this.state.ogp.lat, this.state.ogp.lng];
+        const rb = [this.state.rb.lat, this.state.rb.lng];
+
         return (
-            <div className='.leaflet-container'>
-                <Map center={position} zoom={this.state.zoom}>
+            <div className=".leaflet-container">
+                <Map center={position} zoom={this.state.lez.zoom}>
                     <TileLayer
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={position}>
-                        <Popup>
-                            A pretty CSS3 popup. <br/> Easily customizable.
-                        </Popup>
-                    </Marker>
+                    <CircleMarker
+                        center={position}
+                        radius={50}
+                    >
+                        <Tooltip
+                            direction="right"
+                            offset={[-8, -2]}
+                            opacity={1}
+                        >
+                            <span>
+                                LEZ
+                            </span>
+                        </Tooltip>
+                    </CircleMarker>
+                    <CircleMarker
+                        center={pmi}
+                        radius={5}
+                    >
+                        <Tooltip
+                            direction="right"
+                            offset={[-8, -2]}
+                            opacity={1}
+                        >
+                            <span>
+                                Philip Morris Lietuva
+                            </span>
+                        </Tooltip>
+                    </CircleMarker>
+                    <CircleMarker
+                        center={mars}
+                        radius={10}
+                    >
+                        <Tooltip
+                            direction="right"
+                            offset={[-8, -2]}
+                            opacity={1}
+                        >
+                            <span>
+                                Mars
+                            </span>
+                        </Tooltip>
+                    </CircleMarker>
+                    <CircleMarker
+                        center={gb}
+                        radius={30}
+                    >
+                        <Tooltip
+                            direction="right"
+                            offset={[-8, -2]}
+                            opacity={1}
+                        >
+                            <span>
+                                Glassbel Baltic, UAB
+                            </span>
+                        </Tooltip>
+                    </CircleMarker>
+                    <CircleMarker
+                        center={ogp}
+                        radius={15}
+                    >
+                        <Tooltip
+                            direction="right"
+                            offset={[-8, -2]}
+                            opacity={1}
+                        >
+                            <span>
+                                Orion Global Pet, UAB
+                            </span>
+                        </Tooltip>
+                    </CircleMarker>
+                    <CircleMarker
+                        center={rb}
+                        radius={25}
+                    >
+                        <Tooltip
+                            direction="right"
+                            offset={[-8, -2]}
+                            opacity={1}
+                        >
+                            <span>
+                                RETAL Baltic, UAB
+                            </span>
+                        </Tooltip>
+                    </CircleMarker>
                 </Map>
             </div>
-        )
+        );
     }
 }
