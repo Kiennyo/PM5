656c5c891379097a53ef4cd9f0d50c05dca4819b
Vytautas <Vytautas.Vasiliauskas@outlook.com>
2019-11-16 13:47:08 +0200

    fixed map cpmponent

--- pm5-fe/src/App.js
+++ pm5-fe/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
-import Map from './components/Map';
+import React from "react";
+import "./App.css";
+import Map from "./components/Map";
 
 function App() {
-  return (
-    <div className="App">
-      <Map />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Map />
+        </div>
+    );
 }
 
 export default App;
