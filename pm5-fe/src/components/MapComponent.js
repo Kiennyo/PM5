36339bff310c9ed03a53c5f53c5766577199041e
Vytautas <Vytautas.Vasiliauskas@outlook.com>
2019-11-16 20:08:05 +0200

    transfer

--- pm5-fe/src/components/MapComponent.js
+++ pm5-fe/src/components/MapComponent.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import React from "react";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
 export default class MapComponent extends React.Component {
     state = {
         lat: 51.505,
         lng: -0.09,
-        zoom: 13,
+        zoom: 13
     };
 
     render() {
         const position = [this.state.lat, this.state.lng];
         return (
-            <div className='.leaflet-container'>
+            <div className=".leaflet-container">
                 <Map center={position} zoom={this.state.zoom}>
                     <TileLayer
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -19,11 +19,11 @@ export default class MapComponent extends React.Component {
                     />
                     <Marker position={position}>
                         <Popup>
-                            A pretty CSS3 popup. <br/> Easily customizable.
+                            A pretty CSS3 popup. <br /> Easily customizable.
                         </Popup>
                     </Marker>
                 </Map>
             </div>
-        )
+        );
     }
 }
