0a84c40b1800761e19af00983db50a37e8389a37
Paulius Apulskis <paulius.apulskis@devbridge.com>
2019-11-16 11:46:39 +0200

    setup

--- pm5-be/src/main/java/lt/sikutis/pm5be/controller/PollutionController.java
+++ pm5-be/src/main/java/lt/sikutis/pm5be/controller/PollutionController.java
@@ -1,5 +1,18 @@
 package lt.sikutis.pm5be.controller;
 
-public class PolutionController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class PollutionController {
+
+    @GetMapping
+    public String testApi() {
+        return "test";
+    }
+
+    @GetMapping("/pollution")
+    public String test() {
+        return "{\"hello\":\"world\"}\n";
+    }
 }
