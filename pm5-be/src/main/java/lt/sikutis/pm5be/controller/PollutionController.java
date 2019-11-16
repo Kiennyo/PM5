bf79a178eb199cfaa068342b78f09cc1f5b6547e
Paulius Apulskis <paulius.apulskis@devbridge.com>
2019-11-16 13:54:18 +0200

    map

--- pm5-be/src/main/java/lt/sikutis/pm5be/controller/PollutionController.java
+++ pm5-be/src/main/java/lt/sikutis/pm5be/controller/PollutionController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PollutionController {
 
-    @GetMapping
-    public String testApi() {
-        return "test";
-    }
-
     @GetMapping("/pollution")
     public String test() {
         return "{\"hello\":\"world\"}\n";
--- pm5-fe/src/App.js
+++ pm5-fe/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Map from './components/Map';
+import MapComponent from './components/MapComponent';
 
 function App() {
   return (
     <div className="App">
-      <Map />
+      <MapComponent/>
     </div>
   );
 }
